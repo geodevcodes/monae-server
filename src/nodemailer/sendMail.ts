bfdda9df57e2c225adf78e88e6fd38b3119e4bfa
geodevcode <geodevinstitute@gmail.com>
2025-12-11 08:03:32 +0100

    email transporter updated

--- src/nodemailer/sendMail.ts
+++ src/nodemailer/sendMail.ts
@@ -1,21 +1,4 @@
-// import nodemailer, { Transporter } from "nodemailer";
 import nodemailer, { Transporter } from "nodemailer";
-
-export const sendMail = (): Transporter => {
-  // For Ordinary Gmail account
-  const transporter: Transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465, // SSL
-    secure: true,
-    auth: {
-      user: process.env.MAIL_EMAIL,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
-
-  return transporter;
-};
-
 
 export const createTransporter = (): Transporter => {
   const transporter: Transporter = nodemailer.createTransport({
